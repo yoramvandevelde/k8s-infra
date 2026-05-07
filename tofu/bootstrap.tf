332edd4ade2081c944eeee46634c8869dfbbe41d
yoramvandevelde <yoram@sifft.io>
2026-05-07 12:18:45 +0200

    changes path in bootstrap

--- tofu/bootstrap.tf
+++ tofu/bootstrap.tf
@@ -3,10 +3,10 @@ resource "null_resource" "bootstrap" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      mkdir -p ${path.module}/output
-      tofu output -raw kubeconfig > ${path.module}/output/kubeconfig
-      chmod 600 ${path.module}/output/kubeconfig
-      bash ${path.module}/scripts/bootstrap.sh
+      mkdir -p ${path.module}/../output
+      tofu output -raw kubeconfig > ${path.module}/../output/kubeconfig
+      chmod 600 ${path.module}/../output/kubeconfig
+      bash ${path.module}/../scripts/bootstrap.sh
     EOT
   }
 }
