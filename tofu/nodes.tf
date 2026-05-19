ff7a79ff1fee1341a77ed7a5e63cdfa44fe6d12c
yoramvandevelde <yoram@sifft.io>
2026-05-19 11:08:04 +0200

    chore: reduce control-plane to single node

--- tofu/nodes.tf
+++ tofu/nodes.tf
@@ -2,10 +2,9 @@
 locals {
   nodes = {
     "k8s-cp-1" = { vmid = 301, ip = "10.10.30.1", role = "controlplane" }
-    "k8s-cp-2" = { vmid = 302, ip = "10.10.30.2", role = "controlplane" }
-    "k8s-cp-3" = { vmid = 303, ip = "10.10.30.3", role = "controlplane" }
     "k8s-w-1"  = { vmid = 304, ip = "10.10.30.4", role = "worker" }
     "k8s-w-2"  = { vmid = 305, ip = "10.10.30.5", role = "worker" }
+    "k8s-w-3"  = { vmid = 306, ip = "10.10.30.6", role = "worker" }
   }
 
   cp_nodes     = { for k, v in local.nodes : k => v if v.role == "controlplane" }
